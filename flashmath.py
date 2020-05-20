d5be169baed5c33bfc515087dbb1e6246e76daca
Daniel <dbgodfrey89@msn.com>
2020-05-19 20:21:58 -0600

    Let user choose how many questions to play

--- flashmath.py
+++ flashmath.py
@@ -1,17 +1,31 @@
 import random
 
-n_questions = 10
+def get_int(prompt):
+    try_again = True
+    result = None
+    while try_again:
+        entry = input(prompt)
+        try:
+            result = int(entry)
+            try_again = False
+        except ValueError:
+           print('Invalid entry, try again')
+           try_again = True
+    return result
+
+print('How many questions?')
+n_questions = get_int('>>> ')
 numbers = range(10)
 
 for i in range(n_questions):
     a = random.choice(numbers)
     b = random.choice(numbers)
     print(f'{a} + {b} =')
-    guess = int(input('>>> '))
+    guess = get_int('>>> ')
     answer = a + b
     if guess == answer:
-        print("Correct!")
+        print('Correct!')
     else:
-        print(f"Incorrect. {a} + {b} = {answer}")
+        print(f'Incorrect. {a} + {b} = {answer}')
 
 
