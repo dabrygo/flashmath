f28f27340d70aa90066d6795dec18a6d93e96478
Daniel <dbgodfrey89@msn.com>
2020-05-24 11:13:12 -0600

    Support four-function math

--- flashmath.py
+++ flashmath.py
@@ -1,3 +1,4 @@
+import abc
 import random
 
 def get_int(prompt):
@@ -9,16 +10,47 @@ def get_int(prompt):
            print('Invalid entry, try again')
     return result
 
+# Prompt for operator
+print('What kind of questions?')
+print('1 Addition')
+print('2 Subtraction')
+print('3 Multiplication')
+print('4 Division')
+operator_type = get_int('>>> ')
+if operator_type not in [1, 2, 3, 4]:
+    raise RuntimeError(f'No option for {operator_type}')
+
+# Prompt for questions
 print('How many questions?')
 n_questions = get_int('>>> ')
 numbers = range(10)
 
+operators = ['+', '-', '*', '/']
+operator = operators[operator_type - 1]
 for i in range(n_questions):
-    a = random.choice(numbers)
-    b = random.choice(numbers)
-    print(f'{a} + {b} =')
+    if operator in ['+', '*']:
+        a = random.choice(numbers)
+        b = random.choice(numbers)
+    elif operator == '-':
+        x = random.choice(numbers)
+        y = random.choice(numbers)
+        a = max(x, y)
+        b = min(x, y)
+    elif operator == '/':
+        z = 0
+        while z == 0:
+            x = random.choice(numbers)
+            y = random.choice(numbers)
+            z = x * y
+        a = z
+        use_x = random.choice([0, 1])
+        b = x if use_x else y
+    else:
+        raise NotImplementedError(f'Operator {operator} not yet supported')
+    equation = f'{a} {operator} {b}'
+    print(equation + ' = ')
     guess = get_int('>>> ')
-    answer = a + b
+    answer = eval(equation)
     if guess == answer:
         print('Correct!')
     else:
